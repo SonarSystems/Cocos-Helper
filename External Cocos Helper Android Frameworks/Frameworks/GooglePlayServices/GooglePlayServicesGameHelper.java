415bf8e62aa782b3b27da5ffeaaef67121f05766
Oscar Fernandez <oscar.leif@gmail.com>
2015-11-01 15:17:33 -0500

    Update GooglePlayServicesGameHelper.java
    Google removes this package: (import) com.google.android.gms.appstate.AppStateManager;
    In order to compile this project with the new version of Google Play Services Version : 27+
    it's necessary to erase this lines.

--- External Cocos Helper Android Frameworks/Frameworks/GooglePlayServices/GooglePlayServicesGameHelper.java
+++ External Cocos Helper Android Frameworks/Frameworks/GooglePlayServices/GooglePlayServicesGameHelper.java
@@ -29,7 +29,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 
-import com.google.android.gms.appstate.AppStateManager;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.Api.ApiOptions.NoOptions;
@@ -184,7 +183,7 @@ public class GooglePlayServicesGameHelper implements GoogleApiClient.ConnectionC
     // Should we start the flow to sign the user in automatically on startup? If
     // so, up to
     // how many times in the life of the application?
-    static final int DEFAULT_MAX_SIGN_IN_ATTEMPTS = 0;
+    static final int DEFAULT_MAX_SIGN_IN_ATTEMPTS = 3;
     int mMaxAutoSignInAttempts = DEFAULT_MAX_SIGN_IN_ATTEMPTS;
 
     /**
@@ -301,13 +300,6 @@ public class GooglePlayServicesGameHelper implements GoogleApiClient.ConnectionC
             builder.addApi(Plus.API);
             builder.addScope(Plus.SCOPE_PLUS_LOGIN);
         }
-
-        if (0 != (mRequestedClients & CLIENT_APPSTATE)) 
-        {
-            builder.addApi(AppStateManager.API);
-            builder.addScope(AppStateManager.SCOPE_APP_STATE);
-        }
-
         if (0 != (mRequestedClients & CLIENT_SNAPSHOT)) 
         {
           builder.addScope(Drive.SCOPE_APPFOLDER);
